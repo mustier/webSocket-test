2085ca85e5469056085466ded189798725b48e1b
Mustafa Eryılmaz <mustafaerylmzz@gmail.com>
2025-02-24 22:41:03 +0300

    s4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3411023041"
+    serviceWorkerVersion: "3048140640"
   }
 });
